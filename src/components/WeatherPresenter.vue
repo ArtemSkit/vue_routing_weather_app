c8ed98930402728917a0b9ca6767b47b1c60e944
Artem Skitenko <artemskitenko93@gmail.com>
2022-10-26 14:50:07 -0500

    Added onBeforeUpdate hook

--- src/components/WeatherPresenter.vue
+++ src/components/WeatherPresenter.vue
@@ -1,11 +1,25 @@
 <script setup>
-import { defineProps } from "vue";
+import { defineProps, onBeforeUpdate, ref } from "vue";
 const props = defineProps(["loc", "units"]);
+let lat = 0;
+// let lon = 0;
+let temp = ref(0);
+onBeforeUpdate(async () => {
+  let { street, city, zip } = props.loc.split("|");
+  try {
+    lat = await fetch(
+      `https://geocoding.geo.census.gov/geocoder/locations/address?street=${street}&city=${city}&zip=${zip}&benchmark=Public_AR_Census2020&format=json`
+    );
+    console.log(lat);
+  } catch (err) {
+    console.error(err);
+  }
+});
 </script>
 
 <template>
-  <H1>Weather in {{ props.loc }}</H1>
-  <p>Temp: {{}}</p>
+  <h1>Weather at {{ props.loc.replaceAll("|", ", ") }}</h1>
+  <p>Temp: {{ temp }}</p>
   <p>Wind speed: {{}}</p>
   <p>Precipitation % chanse: {{}}</p>
 </template>
