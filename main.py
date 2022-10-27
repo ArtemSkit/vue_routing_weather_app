d2cb7dd532932a9a7be6ed2db2706fc959f38857
Artem Skitenko <artemskitenko93@gmail.com>
2022-10-27 05:03:45 -0500

    POC complete

--- main.py
+++ main.py
@@ -1,5 +1,102 @@
 import requests
 import json
+from fastapi import APIRouter, FastAPI
+from pydantic import BaseModel
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
-res = requests.get('https://reqres.in/api/users?page=2')
-print(json.dumps(res.json(), indent=2))
+
+class WeatherRequest(BaseModel):
+    street: str
+    city: str
+    state: str | None = None
+    zip_code: int
+    style: str | None = "f"
+
+
+app = FastAPI(redirect_slashes=True)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/weather/")
+async def weather(req_data: WeatherRequest):
+    state_zip = ""
+    if req_data.zip_code > 0:
+        state_zip = f"&zip={req_data.zip_code}"
+    if req_data.state is not None:
+        state_zip = f"&state={req_data.state}" + state_zip
+
+    # Run forward geocoding on the address
+    res = requests.get(
+        f'https://geocoding.geo.census.gov/geocoder/locations/address?street={req_data.street}&city={req_data.city}{state_zip}&benchmark=Public_AR_Census2020&format=json')
+    coords = res.json().get("result", {}).get(
+        "addressMatches", [])
+
+    if not len(coords):
+        print(coords)
+        return {"msg": "Could not obtain coordinates"}
+
+    coords = coords[0].get("coordinates", {})
+
+    if coords:
+        # If we got the coordinates, get forecast office ID and forecast grid coordinates
+        res = requests.get(
+            f'https://api.weather.gov/points/{coords["y"]}%2C{coords["x"]}')
+    else:
+        return {"msg": "Could not obtain coordinates"}
+
+    geo_json = res.json().get("properties", {})
+
+    if not geo_json:
+        return {"msg": "Could not obtain forecast office ID and forecast grid coordinates"}
+
+    # # Uncomment for forecast
+    # # If we got forecast office ID and forecast grid coordinates, get forecast
+    # res = requests.get(
+    #     f'https://api.weather.gov/gridpoints/{geo_json["gridId"]}/{geo_json["gridX"]}%2C{geo_json["gridY"]}')
+
+    # Get station ID
+    res = requests.get(
+        f'https://api.weather.gov/gridpoints/{geo_json["gridId"]}/{geo_json["gridX"]}%2C{geo_json["gridY"]}/stations')
+
+    stations = res.json().get("observationStations", [])
+
+    if not len(stations):
+        return {"msg": "Could not obtain observation stations IDs"}
+
+    station_id = stations[0][stations[0].rindex("/") + 1:]
+
+    if not len(station_id):
+        return {"msg": "Could not obtain observation station ID"}
+
+    # Get the latest observation
+    res = requests.get(
+        f'https://api.weather.gov/stations/{station_id}/observations/latest')
+
+    props = res.json().get("properties", {})
+
+    if not props:
+        return {"msg": "Could not obtain the latest observation"}
+
+    reply = dict()
+
+    reply["temperature"] = props["temperature"]["value"]
+    reply["windSpeed"] = props["windSpeed"]["value"]
+    reply["relativeHumidity"] = props["relativeHumidity"]["value"]
+
+    if reply["temperature"] is not None and req_data.style == "f":
+        reply["temperature"] = reply["temperature"] * 9 / 5 + 32
+
+    return reply
+
+# print(json.dumps(res.json(), indent=2))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- src/components/WeatherPresenter.vue
+++ src/components/WeatherPresenter.vue
@@ -1,16 +1,32 @@
 <script setup>
-import { defineProps, onBeforeUpdate, ref } from "vue";
+import { defineProps, onBeforeMount, ref } from "vue";
 const props = defineProps(["loc", "units"]);
-let lat = 0;
-// let lon = 0;
-let temp = ref(0);
-onBeforeUpdate(async () => {
-  let { street, city, zip } = props.loc.split("|");
+let weatherData = ref({});
+onBeforeMount(async () => {
+  let [street, city, state, zip_code] = props.loc.split("|");
+
   try {
-    lat = await fetch(
-      `https://geocoding.geo.census.gov/geocoder/locations/address?street=${street}&city=${city}&zip=${zip}&benchmark=Public_AR_Census2020&format=json`
-    );
-    console.log(lat);
+    // lat = await fetch(
+    //   `https://geocoding.geo.census.gov/geocoder/locations/address?street=${street}&city=${city}&zip=${zip}&benchmark=Public_AR_Census2020&format=json`
+    // );
+
+    let req = await fetch(`http://localhost:8000/weather/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        street: street,
+        city: city,
+        state: state,
+        zip_code: parseInt(zip_code),
+        style: "f",
+      }),
+    });
+
+    weatherData.value = await req.json();
+
+    console.log(weatherData);
   } catch (err) {
     console.error(err);
   }
@@ -19,7 +35,7 @@ onBeforeUpdate(async () => {
 
 <template>
   <h1>Weather at {{ props.loc.replaceAll("|", ", ") }}</h1>
-  <p>Temp: {{ temp }}</p>
-  <p>Wind speed: {{}}</p>
-  <p>Precipitation % chanse: {{}}</p>
+  <p>Temp: {{ weatherData.temperature }}</p>
+  <p>Wind speed: {{ weatherData.windSpeed }}</p>
+  <p>Relative humidity: {{ weatherData.relativeHumidity }}%</p>
 </template>
